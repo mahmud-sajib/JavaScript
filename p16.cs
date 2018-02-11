08245da09e43e699cc31e9427708074345187a15
mahmud-sajib <32324883+mahmud-sajib@users.noreply.github.com>
2018-02-11 12:21:43 +0600

    Add files via upload

---
+++ p16.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           for(int i =100; i <= 500; i++)
+            {
+                if(i%7==0 && i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
